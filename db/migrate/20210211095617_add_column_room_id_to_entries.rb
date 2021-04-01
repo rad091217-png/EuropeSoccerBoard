2b75d75389c234a7dcabfdcf5a705356d669c9d1
rad091217-png <rad719465@gmail.com>
2021-04-01 17:34:09 +0900

    change migration

--- db/migrate/20210211095617_add_column_room_id_to_entries.rb
+++ db/migrate/20210211095617_add_column_room_id_to_entries.rb
@@ -1,5 +1,4 @@
 class AddColumnRoomIdToEntries < ActiveRecord::Migration[6.0]
   def change
-    add_column :entries, :room_id, :integer
   end
 end
--- db/migrate/20210211095636_add_column_room_id_to_messages.rb
+++ db/migrate/20210211095636_add_column_room_id_to_messages.rb
@@ -1,5 +1,4 @@
 class AddColumnRoomIdToMessages < ActiveRecord::Migration[6.0]
   def change
-    add_column :messages, :room_id, :integer
   end
 end
