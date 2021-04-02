b4e1a589dadf0d522c53adc90d9ba960a7efa405
rad091217-png <rad719465@gmail.com>
2021-04-02 16:09:48 +0900

    to deproy

--- app/models/laliga_post.rb
+++ app/models/laliga_post.rb
@@ -3,6 +3,8 @@ class LaligaPost < ApplicationRecord
   validates :text, presence: true
   mount_uploader :image, ImageUploader
   belongs_to :user
+  has_many :laliga_post_likes
+  has_many :liked_users, through: :laliga_post_likes, source: :user
 
   def correct_user(current_user)
     if id == current_user.id
